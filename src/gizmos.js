1db3db90f1b15ead7fbab2c751fb496f0f01416f
clarkbarrett101 <clarkbarrett101@gmail.com>
2024-01-20 19:58:56 -0700

    scafolding

---
+++ src/gizmos.js
@@ -0,0 +1,14 @@
+let toleranceLevel = 0.1;
+let score = 0;
+
+function setToleranceLevel(){
+
+}
+
+function setModeToRecord(){
+
+}
+
+function setModeToCompare(){
+
+}
---
+++ src/gyro.js
@@ -0,0 +1,19 @@
+class Gyro {
+    constructor() {
+        this.testModifier = 0;
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
+        this.readArray = function(){
+            this.testModifier += 0.1;
+            this.x = Math.sin(this.testModifier);
+            this.y = Math.sin(this.testModifier*2);
+            this.z = Math.sin(this.testModifier/3);
+            return [this.x, this.y, this.z];
+        }
+    }
+}
+
+function compareToGyro(gyro){
+    
+}
