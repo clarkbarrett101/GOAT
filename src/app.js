a9c12b0a9bfb2bb2e52b38bd48334b6a20d77e71
Clark Barrett <clarkbarrett@Clarks-MacBook-Pro.local>
2024-01-24 14:13:38 -0700

    Revert "instructions"
    This reverts commit 83d7b4f23cd50da52ca88b71222d2ac5f570fcdb.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
-import { Gyro } from "./gyro.js";
-import { Reel } from "./reel.js";
-import { ToleranceLevel } from "./gizmos.js";
-
-//UpdateLoop() is the main loop of the program. It checks the current mode and runs the appropriate code. The loop repeats 10 times per second.
-
 const gyro = new Gyro();
-let masterReel = new Reel();
+let masterReel;
 let currentFrame = 0;
 
-const StateMachine = {
+const stateMachine = {
     currentMode: "idle",
     modes: {
         idle: {},
@@ -16,10 +10,8 @@ const StateMachine = {
         comparing: {}
     },
 }
-const stateMachine = Object.create(StateMachine);
 
 function UpdateLoop(){
-  console.log(stateMachine.currentMode);
   switch(stateMachine.currentMode){
     case "idle":
       break;
@@ -30,29 +22,16 @@ function UpdateLoop(){
       break;
     case "comparing":
         if(gyro.isMoving()){
-          let gyroFrame = gyro.readArray();
-          let reelFrame = masterReel.readFrame(currentFrame);
-          let difference = compareGyroFrame(reelFrame,gyroFrame,ToleranceLevel);
+          gyroFrame = gyro.readArray();
+          reelFrame = masterReel.readFrame(currentFrame);
+          let difference = compareGyroFrame(reelFrame,gyroFrame,toleranceLevel);
           changeScore(difference);
           currentFrame++;
         }
       break;
   }  
 }
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-let deltaTime = 0;
 
 while(true){
-    let startTime = Date.now();
     UpdateLoop();
-    gyro.testRotation();
-    let endTime = Date.now();
-    deltaTime = endTime - startTime;
-    if(deltaTime < 100){
-        sleep(100 - deltaTime).then(() => {}  
-        );
-    }
 }
--- src/gizmos.js
+++ src/gizmos.js
@@ -1,8 +1,6 @@
-export { ToleranceLevel};
-
-let ToleranceLevel = 0.1;
+let toleranceLevel = 0.1;
 let score = 0;
-//TODO: Tolerance functions
+
 function increaseToleranceLevel(){
 
 }
@@ -10,7 +8,7 @@ function increaseToleranceLevel(){
 function decreaseToleranceLevel(){
 
 }
-//TODO: Mode functions
+
 function setModeToRecord(){
 
 }
@@ -18,8 +16,6 @@ function setModeToRecord(){
 function setModeToCompare(){
 
 }
-//TODO: Score function
 function changeScore(){
-// this function changes the score by the specified amount then updates the score display to a letter grade {S,A,B,C,D,F}
 
 }
