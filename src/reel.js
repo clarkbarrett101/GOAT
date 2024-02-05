b5e4c2a848f90d983fabc82958e3dc75ef1b6598
Pedro Perillo <pedrofperillo@gmail.com>
2024-02-05 14:45:41 -0700

    First contribution

--- src/reel.js
+++ src/reel.js
@@ -5,9 +5,25 @@ class Reel {
     constructor() {
         this.frames = [];
         //TODO: Read function
-        this.readFrame = function(frameNumber){
-        // this function returns a frame from the reel at the specified frame number
-            
+        class Reel {
+            constructor() {
+                this.frames = [];
+            }
+            // this function returns a frame from the reel at the specified frame number
+            readFrame(frameNumber) {
+                return this.frames[frameNumber];
+            }
+
+            // this function adds a frame to the reel at the specified frame number
+            addFrame(frameNumber, frame) {
+                this.frames[frameNumber] = frame;
+            }
+
+            // this function compares two frames and returns a number representing the difference between them, adjusted by the tolerance level
+            compareGyroFrame(gyroFrame, frameNumber, toleranceLevel) {
+                let difference = Math.abs(this.frames[frameNumber] - gyroFrame);
+                return difference <= toleranceLevel;
+            }
         }
         //TODO: Write function
         this.addFrame = function(frameNumber,frame){
